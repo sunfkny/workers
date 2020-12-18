d3b0f7fe5923848028f29190b0bebe17a040449b
sunfkny <30853461+sunfkny@users.noreply.github.com>
2020-12-19 01:07:23 +0800

    Create cors.js

---
+++ cors.js
@@ -0,0 +1,101 @@
+/*
+ * https://github.com/netnr/workers
+ * 
+ * 2020-08-27
+ * netnr
+ */
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Respond to the request
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+
+    //请求头部、返回对象
+    let reqHeaders = new Headers(request.headers),
+        outBody, outStatus = 200, outStatusText = 'OK', outCt = null, outHeaders = new Headers({
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": reqHeaders.get('Access-Control-Allow-Headers') || "Accept, Authorization, Cache-Control, Content-Type, DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent, X-Requested-With, Token, x-access-token"
+        });
+
+    try {
+        //取域名第一个斜杠后的所有信息为代理链接
+        let url = request.url.substr(8);
+        url = decodeURIComponent(url.substr(url.indexOf('/') + 1));
+
+        //需要忽略的代理
+        if (request.method == "OPTIONS" || url.length < 3 || url.indexOf('.') == -1 || url == "favicon.ico" || url == "robots.txt") {
+            //输出提示
+            outBody = JSON.stringify({
+                code: 0,
+                usage: 'Host/{URL}',
+                source: 'https://github.com/netnr/workers'
+            });
+            outCt = "application/json";
+        } else {
+            //补上前缀 http://
+            if (url.toLowerCase().indexOf("http") == -1) {
+                url = "http://" + url;
+            }
+
+            //构建 fetch 参数
+            let fp = {
+                method: request.method,
+                headers: {}
+            }
+
+            //保留头部其它信息
+            let he = reqHeaders.entries();
+            for (let h of he) {
+                if (!['content-length', 'content-type'].includes(h[0])) {
+                    fp.headers[h[0]] = h[1];
+                }
+            }
+
+            // 是否带 body
+            if (["POST", "PUT", "PATCH", "DELETE"].indexOf(request.method) >= 0) {
+                const ct = (reqHeaders.get('content-type') || "").toLowerCase();
+                if (ct.includes('application/json')) {
+                    fp.body = JSON.stringify(await request.json());
+                } else if (ct.includes('application/text') || ct.includes('text/html')) {
+                    fp.body = await request.text();
+                } else if (ct.includes('form')) {
+                    fp.body = await request.formData();
+                } else {
+                    fp.body = await request.blob();
+                }
+            }
+
+            // 发起 fetch
+            let fr = (await fetch(url, fp));
+            outCt = fr.headers.get('content-type');
+            outStatus = fr.status;
+            outStatusText = fr.statusText;
+            outBody = fr.body;
+        }
+    } catch (err) {
+        outCt = "application/json";
+        outBody = JSON.stringify({
+            code: -1,
+            msg: JSON.stringify(err.stack) || err
+        });
+    }
+
+    //设置类型
+    if (outCt && outCt != "") {
+        outHeaders.set("content-type", outCt);
+    }
+
+    return new Response(outBody, {
+        status: outStatus,
+        statusText: outStatusText,
+        headers: outHeaders
+    })
+
+    // return new Response('OK', { status: 200 })
+}
